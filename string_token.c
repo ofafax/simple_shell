4358ece9c3e7b8307024f6b31db974cf099ac4e2
ofafax <98403358+ofafax@users.noreply.github.com>
2023-04-27 00:47:42 +0300

    Update string_token.c

--- string_token.c
+++ string_token.c
@@ -1,39 +1,41 @@
 #include "shell.h"
+
 /**
- * _strtok - separates strings with delimiters
- * @line: It´s pointer to array we receive in getline.
- * @delim: It´s characters we mark off string in parts.
- * Return: A pointer to the created token
+ * _strtok - separates a string with delimiters
+ * @line: It is a pointer to an array that we receive in getline
+ * @delim: It is characters that we mark off strings in parts
+ * Return: the pointer to the created token
 */
+
 char *_strtok(char *line, char *delim)
 {
 	int v;
-	static char *s;
+	static char *sttc;
 	char *c;
 
 	if (line != NULL)
 		s = line;
-	for (; *s != '\0'; s++)
+	for (; *sttc != '\0'; sttc++)
 	{
 		for (v = 0; delim[v] != '\0'; v++)
 		{
-			if (*s == delim[v])
+			if (*sttc == delim[v])
 			break;
 		}
 		if (delim[v] == '\0')
 			break;
 	}
-	c = s;
+	c = sttc;
 	if (*c == '\0')
 		return (NULL);
-	for (; *s != '\0'; s++)
+	for (; *sttc != '\0'; sttc++)
 	{
 		for (v = 0; delim[v] != '\0'; v++)
 		{
-			if (*s == delim[v])
+			if (*sttc == delim[v])
 			{
-				*s = '\0';
-				s++;
+				*sttc = '\0';
+				sttc++;
 				return (c);
 			}
 		}
