41c885356613fde7ae81d91abcda9a011e010816
ofafax <98403358+ofafax@users.noreply.github.com>
2023-04-27 00:40:13 +0300

    Update strcpy.c

--- strcpy.c
+++ strcpy.c
@@ -1,12 +1,12 @@
 #include "shell.h"
 
 /**
- * _strcpy - copies the string pointed to by src,
- * including the terminating null byte (\0),
+ * _strcpy - copies the string pointed to by pointer src
+ * includng the terminating null byte char ('\0')
  * to the buffer pointed to by dest
- * @dest: copy source to this buffer
- * @src: this is the source to copy
- * Return: copy of original source
+ * @dest: copies source to this buffer
+ * @src: source to be copied
+ * Return: original source copy
  */
 
 char *_strcpy(char *dest, char *src)
