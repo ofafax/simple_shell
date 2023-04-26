51d9f414f0f8d5e2fbd3d3da7620ba07ede4a41b
ofafax <98403358+ofafax@users.noreply.github.com>
2023-04-27 00:20:23 +0300

    Update builtin_environment.c
    update

--- builtin_environment.c
+++ builtin_environment.c
@@ -1,9 +1,9 @@
 #include "shell.h"
 
 /**
- * builtin_env - shows the environment where the shell runs
- * @data: struct for the program's data
- * Return: zero if sucess, or other number if its declared in the arguments
+ * builtin_env - shows the environmnt where the shell runs
+ * @data: struct for the programs data
+ * Return: zero if success, or other number if its declard in the argmnts
  */
 int builtin_env(data_of_program *data)
 {
@@ -45,9 +45,9 @@ int builtin_env(data_of_program *data)
 }
 
 /**
- * builtin_set_env - ..
- * @data: struct for the program's data
- * Return: zero if sucess, or other number if its declared in the arguments
+ * builtin_set_env - ..shows set env
+ * @data: struct for the programs data
+ * Return: zero if sucess, or other number if its declard in the argmnts
  */
 int builtin_set_env(data_of_program *data)
 {
