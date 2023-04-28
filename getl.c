b617c15cbf3c87c83c52fc760fe2656e52aac3f4
Dottielaleti <laletidottie@gmail.com>
2023-04-28 10:58:10 -0700

    getl.c

--- getl.c
+++ getl.c
@@ -5,7 +5,7 @@ void assign_lineptr(char **lineptr, size_t *t, char *buff, size_t q);
 ssize_t _getline(char **lineptr, size_t *n, FILE *stream);
 
 /**
- * _realloc - Reallocates memory using malloc and free
+ * _realloc - Reallocates memory using malloc and free.
  * @ptr: A pointer to the memory previously allocated
  * @old_size: The size in bytes of the allocated space for a ptr
  * @new_size: The size in bytes for the new memory block to
